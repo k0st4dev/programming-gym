0393939f12bfa13adb6554d7e38a035bbe4a8d40
Veljko Kostović <veljko.kostovic@petrol.si>
2026-09-15 17:02:17 +0200

    dan1

--- programming-gym/problems/Dan1-Problem1-FizzBuzz.js
+++ programming-gym/problems/Dan1-Problem1-FizzBuzz.js
@@ -1,4 +1,4 @@
-
+/* //1. problem FizzBuzz
 for(let a=1;a<=100;a++){
   if((a%3===0)&&(a%5===0)){
     console.log("FizzBuzz");
@@ -10,4 +10,31 @@ for(let a=1;a<=100;a++){
   }
   else console.log(a);
 
+}
+*/
+
+/*//2. problem
+for(let a=1;a<=50;a++){
+  if((a%4===0)&&(a%6===0)){
+    console.log("EvenBuzz");
+  }else if(a%4===0){
+    console.log("Even");
+  }else if(a%6===0){
+    console.log("Buzz")
+  }
+    else {
+    console.log(a);
+  }
+}
+*/
+
+
+//Problem 3
+
+function zbir(n) {
+  let tmp=0;
+  for (let a=1;a<=n;a++){   
+    tmp+=a;
+  }
+  return tmp;
 }
